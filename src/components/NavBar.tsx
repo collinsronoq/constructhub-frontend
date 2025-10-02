f2f5989955f63bd6351c6626113fae825275d0b5
collinsronoq <ronoqkabarak@gmail.com>
2025-10-03 02:25:00 +0300

    polished up the authlayout to make it more appealing. added navbar at the landing page to enable theme togglinng

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,32 +1,78 @@
+import { useState, useEffect } from "react"
 
 
-// const NavBar = () =>{
+
+const NavBar = () =>{
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+  const [isDark, setIsDark ] = useState<boolean>(false);
+
+  const isLogged:boolean = false;
+
+  useEffect(() => {
+    setIsDark(document.documentElement.classList.toggle("dark"));
+  }, []);
+
+  const toggleDarkMode = () =>{
+    document.documentElement.classList.toggle("dark");
+    setIsDark(!isDark)
+  }
+
   
-//   return(
+  
+  return(
     
-//     <header className="flex items-center justify-between h-16 px-4 bg-surface-light dark:bg-surface-dark shadow-md">
-//       {/* Hamburger (mobile only) */}
-//       <button
-//         onClick={() => setSidebarOpen(!sidebarOpen)}
-//         className="md:hidden text-gray-600 dark:text-gray-300"
-//       >
-//         ☰
-//       </button>
+    <header className="flex items-center justify-between h-16 px-4 bg-surface-light dark:bg-surface-dark shadow-md">
+      
 
-//       <h1 className="text-xl font-semibold">Dashboard</h1>
+      <div className="flex flex-row space-x-2">
+        <div className="text-gray-600 dark:text-gray-300">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-shovel-icon lucide-shovel w-4 h-4 md:w-6 md:h-6"><path d="M21.56 4.56a1.5 1.5 0 0 1 0 2.122l-.47.47a3 3 0 0 1-4.212-.03 3 3 0 0 1 0-4.243l.44-.44a1.5 1.5 0 0 1 2.121 0z"/><path d="M3 22a1 1 0 0 1-1-1v-3.586a1 1 0 0 1 .293-.707l3.355-3.355a1.205 1.205 0 0 1 1.704 0l3.296 3.296a1.205 1.205 0 0 1 0 1.704l-3.355 3.355a1 1 0 0 1-.707.293z"/><path d="m9 15 7.879-7.878"/></svg>
+        </div>
+        <h1 className="text-sm md:text-xl font-semibold text-gray-900 dark:text-blue-200">CONSTRUCT HUB</h1>
+      </div>
+      
 
-//       {/* Right side: Dark mode toggle + User */}
-//       <div className="flex items-center space-x-4">
-//         <button
-//           onClick={() =>
-//             document.documentElement.classList.toggle("dark")
-//           }
-//           className="px-2 py-1 rounded bg-gray-200 dark:bg-gray-700"
-//         >
-//           🌙
-//         </button>
-//         <div className="w-8 h-8 bg-gray-400 rounded-full"></div>
-//       </div>
-//     </header>
-//   )
-// }
+      {/* Right side: Dark mode toggle + User */}
+      <div className="flex items-center space-x-4">
+        <button
+          onClick={toggleDarkMode}
+          className="px-2 py-2 text-gray-600 dark:text-gray-300"
+        >
+          {!isDark ? (
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-sun-icon lucide-sun  w-4 h-4 md:w-6 md:h-6"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/>
+              </svg>
+            </div>):
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-moon-icon lucide-moon w-4 h-4 md:w-6 md:h-6"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>
+            </div>
+          }
+        </button>
+        <div className="px-2 py-2 text-gray-600 dark:text-gray-300  items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"    stroke="currentColor" className="w-4 h-4 md:w-6 md:h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+          </svg>
+
+        </div>
+        {/* Hamburger (mobile only) */}
+        {/* wafaa kuona kama mtumiaji anaruhusa ndiposa haweze kuona hili */}
+        {isLogged && (
+          <button
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+          className="md:hidden text-gray-600 dark:text-gray-300"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+
+            {/* ☰ */}
+          </button>
+
+        )}
+        
+      </div>
+    </header>
+  )
+}
+
+export default NavBar
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -27,6 +27,7 @@
 import { useState } from "react";
 import LoginCard from "../components/LoginCard";
 import SignUpCard from "../components/SignUpCard";
+import NavBar from "../components/NavBar";
 
 interface AuthLayoutProps {
   description: string;
@@ -36,62 +37,79 @@ const AuthLayout = ({ description }: AuthLayoutProps) => {
   const [activeModal, setActiveModal] = useState<"login" | "signup" | null>(null);
 
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center bg-gray-50 p-6">
-      {/* Title */}
-      <h1 className="text-4xl font-extrabold text-blue-600 mb-6">ConstructHub</h1>
+    <>
+      <NavBar />
+      <div className="grid grid-cols-1 md:grid-cols-2  min-h-screen  bg-background-light dark:bg-background-dark p-1">
+        <div className="p-6">
+          <h1 className="text-2xl md:text-4xl font-extrabold text-brand-light dark:text-brand-dark mb-4">ConstructHub... Your Construction Companion</h1>
+          <p className="text-gray-900 dark:text-gray-100 mb-6 md:text-xl">
+            Get started now, sign up below to get accurate cost estimates, verified technicians and trusted vendors around you as a builder.
+            
+          </p>
+          
+          <p className="text-gray-900 dark:text-gray-100 mb-6 md:text-xl">
+            Already have an account log in view your estimates and the technicians and vendors near you.
+          </p>
 
-      {/* Buttons */}
-      <div className="flex space-x-4 mb-6">
-        <button
-          onClick={() => setActiveModal("login")}
-          className="px-6 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition"
-        >
-          Login
-        </button>
-        <button
-          onClick={() => setActiveModal("signup")}
-          className="px-6 py-2 border border-blue-600 text-blue-600 rounded-lg hover:bg-blue-50 transition"
-        >
-          Sign Up
-        </button>
-      </div>
-
-      {/* Description */}
-      <div className="max-w-xl text-center text-gray-700 text-lg leading-relaxed">
-        <p className="mb-4">
-          ConstructHub is your all-in-one construction assistant. It helps
-          builders estimate project costs, source quality materials, and connect
-          with verified technicians across Kenya.
-        </p>
-        <p>
-          Whether you’re a <span className="font-semibold">builder</span>,
-          <span className="font-semibold"> vendor</span>, or
-          <span className="font-semibold"> technician</span>, ConstructHub
-          empowers you with the tools to plan, source, and execute construction
-          projects confidently.
-        </p>
-        <p>{description}</p>
-      </div>
-
-      {/* Modal Overlay */}
-      {activeModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center p-4 z-50">
-          <div className="bg-white w-full max-w-md rounded-xl shadow-lg p-6 relative">
-            {/* Close Button */}
+          {/* Buttons */}
+          <div className="flex space-x-4 mb-6">
             <button
-              onClick={() => setActiveModal(null)}
-              className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
+              onClick={() => setActiveModal("login")}
+              className="px-6 py-2 border border-blue-600 bg-brand-light dark:bg-background-dark text-white rounded-lg shadow hover:bg-blue-700 dark:hover:bg-blue-900 transition"
             >
-              ✕
+              Login
             </button>
-
-            {/* Show card */}
-            {activeModal === "login" && <LoginCard />}
-            {activeModal === "signup" && <SignUpCard />}
+            <button
+              onClick={() => setActiveModal("signup")}
+              className="px-6 py-2 border border-blue-600 text-white bg-brand-light dark:bg-background-dark rounded-lg hover:bg-blue-700 dark:hover:bg-blue-700 transition"
+            >
+              Sign Up
+            </button>
           </div>
+
+          {/* Description */}
+          <div className="max-w-xl text-gray-900 dark:text-gray-200 text-lg md:text-xl leading-relaxed">
+            <p className="mb-4">
+              ConstructHub is an all-in-one digital platform designed to revolutionize the construction experience for everyone involved — builders, vendors, technicians, and project owners.
+              We simplify construction planning and execution by bringing together cost estimation tools, vendor marketplaces, and trusted professional services in one place.
+            </p>
+            <p className="py-4 mb-4">
+              <span className="font-semibold">For Builders & Project Owners</span>
+              <br />
+              Instantly estimate project costs with transparent breakdowns.
+
+              Compare real-time prices from multiple vendors.
+
+              Access a network of verified technicians (electricians, plumbers, masons, etc.) to bring your project to life.
+            </p>
+            <p>{description}</p>
+          </div>
+
+          {/* Modal Overlay */}
+          {activeModal && (
+            <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center p-4 z-50">
+              <div className="bg-white w-full max-w-md rounded-xl shadow-lg p-6 relative">
+                {/* Close Button */}
+                <button
+                  onClick={() => setActiveModal(null)}
+                  className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
+                >
+                  ✕
+                </button>
+
+                {/* Show card */}
+                {activeModal === "login" && <LoginCard />}
+                {activeModal === "signup" && <SignUpCard />}
+              </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
+        <div className="">
+          <img src="src/assets/image_1.jpg" className="aspect-square object-fill"></img>
+        </div>
+      </div>
+    </>
+    
   );
 };
 
