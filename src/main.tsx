3b117bb32b3408f58de6d2186c6c55d7425d06a8
collinsronoq <ronoqkabarak@gmail.com>
2025-12-02 21:46:02 +0300

    Finsihed up frontend mostly , now moving to backend

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import "./index.css"
-
 import DashboardLayout from "./layouts/DashboardLayout"
 import Dashboard from "./pages/BuilderDashboard"
 import VendorProfile from "./pages/VendorProfile"
